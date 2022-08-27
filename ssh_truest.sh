05fe4063ead80f1cb9a29c5d0d695af1d59639c0
qq42877331 <85149088+qq42877331@users.noreply.github.com>
2022-08-28 02:21:15 +0800

    Update ssh_truest.sh

--- ssh_truest.sh
+++ ssh_truest.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-'''
-这个脚本会自己生成本地ssh pubkey，并根据hosts内容，找到相应主机并同步密钥过去，完全后ssh可以免密码登陆
-  1、首先在/etc/hosts里填好ip与host的对应关系
-  2、host的密码都是统一的
-'''
-
-[ -f ~/.ssh/id_rsa ] || ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa
-
-echo -e "\033[31m 请把节点 [IP]、[计算机名] 添加到/etc/hosts\n \033[0m"
-
-read -p "请输远端节点的root密码，要求全部节点使用同一密码：" nodepw
+nodepw=`cat ./nodepw `
 
 #定义一个put_sshkey方法
 put_sshkey(){
@@ -23,8 +13,11 @@ put_sshkey(){
         }"
 }
 
+#生成本地ssh pubkey 
+[ -f ~/.ssh/id_rsa ] || ssh-keygen -t rsa -P '' -f ~/.ssh/id_rsa
+
+
 for i in `cat /etc/hosts |grep -v localhost | awk '{print $2}'`
 do
-                put_sshkey $i $nodepw
+	put_sshkey $i $nodepw
 done
-
